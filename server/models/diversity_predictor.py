b675b77a3c21cb2744e9155c30263853d793ce08
Brody Jackson <brodyjackson1@gmail.com>
2020-02-15 13:15:45 -0700

    Predictive layer working for overall diversity and individual lifestyle factors

--- server/models/diversity_predictor.py
+++ server/models/diversity_predictor.py
@@ -3,24 +3,21 @@ import numpy as np
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
 from sklearn import linear_model
-df = pd.read_csv('/Users/brody.jackson/Desktop/07-taxa/100nt/ag-cleaned_L2.txt', delimiter="\t", low_memory=False)
 
 predictive_columns = [
-    'AGE_YEARS',
-    # 'HEIGHT_CM',
-    'WEIGHT_KG',
-    'VIOSCREEN_VEGETABLE_SERVINGS',
-    'VIOSCREEN_FRUIT_SERVINGS',
-    'VIOSCREEN_ALCOHOL_SERVINGS',
-    'VIOSCREEN_CALORIES',
-    'VIOSCREEN_TOTSUGAR',
-    'VIOSCREEN_FAT',
-    'VIOSCREEN_PROTEIN',
-    'VIOSCREEN_CARBO'
-]
+        'AGE_YEARS',
+        # 'HEIGHT_CM',
+        'WEIGHT_KG',
+        'VIOSCREEN_VEGETABLE_SERVINGS',
+        'VIOSCREEN_FRUIT_SERVINGS',
+        'VIOSCREEN_ALCOHOL_SERVINGS',
+        'VIOSCREEN_CALORIES',
+        'VIOSCREEN_TOTSUGAR',
+        'VIOSCREEN_FAT',
+        'VIOSCREEN_PROTEIN',
+        'VIOSCREEN_CARBO'
+    ]
 
 categorical_columns = [
     'FERMENTED_PLANT_FREQUENCY',
@@ -30,47 +27,68 @@ categorical_columns = [
     'SLEEP_DURATION'
 ]
 
-for column in categorical_columns:
-    df[column] = df[column].astype('category')
-    df[f'{column}_cat'] = df[column].cat.codes
-    predictive_columns.append(f'{column}_cat')
+predictive_target_columns = [
+    'k__Bacteria;p__Proteobacteria',
+    'k__Bacteria;p__Actinobacteria',
+    'k__Bacteria;p__Bacteroidetes',
+    'k__Bacteria;p__Cyanobacteria',
+    'k__Bacteria;p__Firmicutes',
+    'k__Bacteria;p__Tenericutes',
+    'k__Bacteria;p__Verrucomicrobia',
+    'k__Bacteria;p__Fusobacteria'
+]
 
-test = df['ANTIBIOTIC_HISTORY_cat']
+lifestyle_factor_averages = {}
+phylum_diversity_averages = {}
 
-for column in predictive_columns:
-    df = df[~df[column].isin(['Unknown', 'Unspecified'])]
-
-target = pd.DataFrame(df['k__Bacteria;p__Proteobacteria'])
-
-df[predictive_columns] = df[predictive_columns].apply(pd.to_numeric, errors='coerce', axis=1)
-
-df = df.fillna(df.mean())
-print(pd.get_dummies(df['ANTIBIOTIC_HISTORY']))
-X = df[predictive_columns]
-Y = pd.to_numeric(target['k__Bacteria;p__Proteobacteria'], errors='coerce')
-
-model = sm.OLS(Y, X, missing='drop').fit()
-predictions = model.predict(X)
-print(model.summary())
-print(predictions)
-
-fig = plt.figure()
-# fig.subplots_adjust(left=None, right=None, , wspace=None, hspace=None )
-# fig.tight_layout()
-
-i = 1
-for column in predictive_columns:
-    axis = fig.add_subplot(3,3,i)
-    axis.yaxis.label.set_visible(False)
-    sns.regplot(x=df[column], y=df['k__Bacteria;p__Proteobacteria'], ax=axis, marker=".")
-    i += 1
+def generate_figures(target, df):
+    fig = plt.figure(target)
+    i = 1
+    for column in predictive_columns:
+        axis = fig.add_subplot(3, 5, i)
+        axis.yaxis.label.set_visible(False)
+        sns.regplot(x=df[column], y=df[target], ax=axis, marker=".")
+        i += 1
 
 
-lm = linear_model.LinearRegression()
-skmodel = lm.fit(X,Y)
-skpredictions = lm.predict(X)
-print(skpredictions[0:5])
-print(lm.score(X,Y))
+def generate_models():
 
-# exit(0)
-# print(df['k__Bacteria;p__Verrucomicrobia'])
+    df = pd.read_csv('/Users/brody.jackson/Desktop/07-taxa/100nt/ag-cleaned_L2.txt', delimiter="\t", low_memory=False)
+
+    for column in categorical_columns:
+        df[column] = df[column].astype('category')
+        df[f'{column}_cat'] = df[column].cat.codes
+        predictive_columns.append(f'{column}_cat')
+
+    for column in predictive_columns:
+        df = df[~df[column].isin(['Unknown', 'Unspecified'])]
+
+    df[predictive_columns] = df[predictive_columns].apply(pd.to_numeric, errors='coerce', axis=1)
+    df = df.fillna(df.mean())
+    X = df[predictive_columns]
+
+    for column in predictive_columns:
+        if 'cat' in column:
+            lifestyle_factor_averages[column] = df[column].mean().round(0)
+        else:
+            lifestyle_factor_averages[column] = df[column].mean()
+
+    regression_models = {}
+    for target in predictive_target_columns:
+
+        target_data = pd.DataFrame(df[target])
+        Y = pd.to_numeric(target_data[target], errors='coerce')
+        phylum_diversity_averages[target] = Y.mean()
+
+        model = sm.OLS(Y, X, missing='drop').fit()
+        regression_models[target] = model
+
+        predictions = model.predict(X)
+        print(model.summary())
+        print(predictions)
+
+        generate_figures(target, df)
+
+
+if __name__ == '__main__':
+    generate_models()
